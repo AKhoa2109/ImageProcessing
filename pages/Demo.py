d1a6e82df512b568ba71a798b9f4e9afd23a366b
ledinhloc <ledinhloc7@gmail.com>
2025-04-30 14:42:18 +0700

    done chuong 3

---
+++ pages/Demo.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import argparse
+import numpy as np
+import cv2 as cv
+import joblib
+
+
+
+if __name__ == '__main__':
+    st.title('Page demo')
+    # c1,c2 = st.columns(2)
+    # if c1.button('Real Time'):
+    #     handlerCamera('')
+    # if c2.button('Video'):
+    #     handlerVideo()
