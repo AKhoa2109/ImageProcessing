8c8ec18406a8fc4ace11cda5c77070e9589ddc42
anhkhoa2109.it@gmail.com <anhkhoa2109.it@gmail.com>
2025-05-01 10:52:24 +0700

    Thêm nhận dạng trái cây (final)

---
+++ NhanDangTraiCay/ChuanHoaAnh/chuan_hoa_anh_640x640.py
@@ -0,0 +1,25 @@
+import os
+import cv2
+import numpy as np
+path = 'NhanDangTraiCay/Image/Xoai/'
+path_dest = 'NhanDangTraiCay/TraiCay640x640/Xoai/'
+lst_dir = os.listdir(path)
+dem = 0
+for filename in lst_dir:
+    print(filename)
+    fullname = path + filename
+    imgin = cv2.imread(fullname, cv2.IMREAD_COLOR)
+    # M: width, N: height, C: channel: 3
+    M, N, C = imgin.shape
+    if M < N:
+        imgout = np.zeros((N, N, C), np.uint8) + 255
+        imgout[:M, :N, :] = imgin
+    elif M > N:
+        imgout = np.zeros((M, M, C), np.uint8) + 255
+        imgout[:M, :N, :] = imgin
+    else:
+        imgout = imgin.copy()
+    imgout = cv2.resize(imgout, (640, 640))
+    fullname_dest = path_dest + 'Xoai_%03d.jpg' % dem
+    dem+=1
+    cv2.imwrite(fullname_dest, imgout)
